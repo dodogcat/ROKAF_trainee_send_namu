e931e7a950e740057c436b3fb1210def2feeb028
dodogcat <42289505+dodogcat@users.noreply.github.com>
2022-06-25 22:33:18 +0900

    Update save_wiki.py

--- save_wiki.py
+++ save_wiki.py
@@ -2,34 +2,30 @@ import json
 import sqlalchemy
 import pymysql
 
-
-# with open("E:\\나무위키\\namuwiki_20210301.json", "r", encoding="UTF8") as st_json:
-#     st_python = json.load(st_json)
-
-# print(st_python)
-
-
+# namespace의 끝을 확인합니다. 다음 데이터인 title의 앞부분인 잡다한 정보까지 받습니다.
 def check_NS_End(str):
     if str.endswith(',"title":"') == True:
         return True
     else:
         return False
-
+    
+# tit;e의 끝을 확인합니다. 다음 데이터인 text의 앞부분인 잡다한 정보까지 받습니다.
 def check_TITLE_End(str):
     if str.endswith('","text":"') == True:
         return True
     else:
         return False
 
+# text의 끝을 확인합니다. 다음 데이터인 contribute의 앞부분인 잡다한 정보까지 받습니다.
 def check_TEXT_End(str):
     if str.endswith('","contributors":[') == True:
         return True
     else:
         return False
 
+# 파일이 용량이 크기 때문에 조금씩 읽어야 한다.
 f = open("E:\\나무위키\\namuwiki_20210301.json", 'r', encoding="UTF-8")
 
-# print(f.readline())
 c = f.read(1)
 mode = 'namespace'
 namespace = ''
@@ -37,6 +33,8 @@ title = ''
 text = ''
 
 while c!='':
+    # 한 루프마다 하나의 JSON 객체의 정보 추출
+    # namespace 까지의 잡다한 정보 제거
     c = f.read(13)
 
     if mode == 'namespace':
@@ -44,6 +42,7 @@ while c!='':
             c = f.read(1)
             namespace += c
             if(check_NS_End(namespace) == True):
+                # 잡다한 정보 제거
                 namespace = namespace[0:-10]
                 mode = 'title'
                 break
@@ -53,6 +52,7 @@ while c!='':
             c = f.read(1)
             title += c
             if(check_TITLE_End(title) == True):
+                # 잡다한 정보 제거
                 title = title[0:-10]
                 mode = 'text'
                 break
@@ -62,17 +62,18 @@ while c!='':
             c = f.read(1)
             text += c
             if(check_TEXT_End(text) == True):
+                # 잡다한 정보 제거
                 text = text[0:-18]
                 mode = 'namespace'
                 break
 
     # delete contributes
-
     while c != ']':
         c = f.read(1)
     c = f.read(2)
-
-    conn = pymysql.connect(host='127.0.0.1', user='root', password='!tnthd001', db='namu', charset='utf8mb4')
+    
+    # 대충 sql에 저장하기
+    conn = pymysql.connect(host='127.0.0.1', user='root', password='암호', db='namu', charset='utf8mb4')
 
     cur = conn.cursor()
 
